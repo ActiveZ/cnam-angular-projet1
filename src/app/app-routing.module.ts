66c7e6f1ce1bd12527c25f33c8092ff8667eb52a
ActiveZ <activez.git@gmail.com>
2020-10-19 14:44:57 +0200

    routing ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
-  { path: 'dashboard', component: dashboardComponent}
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '**', component: AuthComponent}
 ];
 
 @NgModule({
